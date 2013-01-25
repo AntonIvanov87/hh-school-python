0f3f622e20acee71e90637e9f53b38bac4478375
Anton Ivanov <keyplayer@yandex.ru>
2013-01-25 22:18:10 +0400

    Added 4_ireduce.py. Made 2_lazy.py executable

---
+++ 4_ireduce.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+def ireduce(func, iterable, init=None):
+
+	iterator = iter(iterable)
+	
+	if init == None:
+		try:
+			init = next(iterator)
+		except StopIteration:
+			raise TypeError('Empty iterable and no init value!')
+	
+	acc = init
+
+	for i in iterator:
+		acc = func(acc, i)
+		yield acc
+
+
+if __name__ == '__main__':
+
+	print 'ireduce(add, [1, 2, 3]):'
+	l = [1, 2, 3]
+	for i in ireduce(lambda a, b: a+b, l):
+		print i
+
+	print
+	print 'ireduce(add, ["a", "b", "c"])'
+	l = ['a', 'b', 'c']
+	for i in ireduce(lambda a, b: a+b, l):
+		print i
