020baca3fdfdf8b130a8ea8886d1ee3fc01f9444
Anton Ivanov <keyplayer@yandex.ru>
2013-01-27 16:31:08 +0400

    Made 1_curry to support different kinds of args

--- 1_curry.py
+++ 1_curry.py
@@ -1,20 +1,47 @@
 #!/usr/bin/python
 
+"""Decorator that makes function possible to curry"""
 class curry:
 	
-	def __init__(self, func, prevArgs=()):
+	def __init__(self, func, prevPargs=(), prevKargs={}):
 
 		self.func = func
-		self.funcNumOfArgs = func.func_code.co_argcount
-		self.prevArgs = prevArgs
+		func_code = func.func_code
 
-	def __call__(self, *args):
+		funcDefArgCount = 0 if func.func_defaults is None else len(func.func_defaults)
+		funcMandatoryArgCount = func_code.co_argcount - funcDefArgCount
+		self.funcMandatoryArgs = func_code.co_varnames[:funcMandatoryArgCount]
 
-		allArgs = self.prevArgs + args
-		if len(allArgs) < self.func.func_code.co_argcount:
-			return curry(self.func, allArgs)
+		self.prevPargs = prevPargs
+		self.prevKargs = prevKargs
+
+
+	def __call__(self, *pargs, **kargs):
+
+		allPargs = self.prevPargs + pargs
+		allKargs = self.prevKargs.copy()
+		allKargs.update(kargs)
+
+		if self.canBeCalled(allPargs, allKargs):
+			return self.func(*allPargs, **allKargs)
 		else:
-			return self.func(*allArgs)
+			return curry(self.func, allPargs, allKargs)
+
+
+	def canBeCalled(self, allPargs, allKargs):
+
+		# If there are enough positional args passed - the function can be called
+		if len(allPargs) >= len(self.funcMandatoryArgs):
+			return True
+
+		# If other mandatory args are in kargs - the function can be called
+		mandatoryArgsMaybeInKargs = self.funcMandatoryArgs[len(allPargs):]
+		for mandatoryArg in mandatoryArgsMaybeInKargs:
+			if mandatoryArg not in allKargs:
+				return False
+
+		# Not enough args
+		return True
 
 
 if __name__ == '__main__':
@@ -38,3 +65,21 @@ if __name__ == '__main__':
 	print 'Calling result = add1(4)'
 	result = add1(4)
 	print 'result = ', result
+
+	@curry
+	def func(a, b, c=3, d=4, *pargs, **kargs):
+		print a, b, c, d, pargs, kargs
+
+	print
+	print 'Calling func(1, 2, 3, 4, 5, x=6)'
+	func(1, 2, 3, 4, 5, x=6)
+
+	print 'Calling func1 = func(1)'
+	func1 = func(1)
+	print 'func1 =', func1
+
+	print 'Calling func1(2, 3, 4, 5, x=6)'
+	func1(2, 3, 4, 5, x=6)
+
+	print 'Calling func1(2)'
+	func1(2)
