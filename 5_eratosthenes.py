eee011f7f9db251ef212643552e9cb35840c49b3
Anton Ivanov <keyplayer@yandex.ru>
2013-01-27 23:51:27 +0400

    Added few comments to 5_eratosthenes to make review simpler

--- 5_eratosthenes.py
+++ 5_eratosthenes.py
@@ -18,6 +18,14 @@ def primes(max):
 
 			# this = is legal - it is just a synonym to make next expression more readable
 			new_non_primes = non_primes.union(set(range(numbers[0]*2, max, numbers[0])))
+			# we can do better:
+			# now if we see 2 we add 4, 6, 8... into the new_non_primes
+			# we can add only 4 instead and remember that 4 was added because of prime 2
+			# later, when we see 4, we can substitue it with 6 instead, etc.
+			# thus on every recursion we can maintain only minimum number of non_primes required
+			# but it will be pretty hard to implement this without statements
+			# thus I decided to leave this version
+
 			return numbers[0:1] + sieve(numbers[1:], new_non_primes, max)
 
 	return sieve(range(2, max), set(), max)
