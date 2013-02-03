8020bfae8162354086ccb23a8c97c78d75658a03
Anton Ivanov <keyplayer@yandex.ru>
2013-02-03 19:56:08 +0400

    Added lazy_compile to 7_graph.py

--- 7_graph.py
+++ 7_graph.py
@@ -2,77 +2,94 @@
 
 # fixed all issues:
 # decided to use func.__name__ to determine what function should be called
-# made detection of cycles
+# added detection of cycles
+# added lazy_compile
+
+
+class CyclicDependency(Exception):
+	pass
+
+
+class MissingParameter(Exception):
+	pass
+
 
 def simple_compile(functions):
 	"""
 	Returns a function compute_all.
 	The compute_all accepts a map of input parameters
 	and returns a map of all parameters that can be computed with given functions.
-	Each function parameter name must match the name of the function that computes this param.
-	In case of cyclic dependancies - ValueError is raised.
+	Each function parameter name must match the name of the function that computes this parameter.
+	Can raise CyclicDependency and MissingParameter.
 	"""
 
+	name_to_func = map_names_to_functions(functions)
 	graph = make_graph(functions)
-	top_sorted = reversed_top_sort(graph)
+	top_order = reversed_top_order(graph, graph.keys())
 	# graph is a dict {function name: function arguments}
 	# in reversed topoligical order each argument goes before its' function
 	# thus we have to call functions precisely in this order 
 
-	name_to_func = dict([(func.__name__, func) for func in functions])
-
 	def compute_all(input_map):
 
-		output_map = input_map.copy()
-
-		for param in top_sorted:
-
-			# if value of the parameter was already in input_map - continue
-			if param in output_map:
-				continue
-
-			function = name_to_func[param]
-			args_names = graph[param]
-			args_to_values = dict([(arg_name, output_map[arg_name]) for arg_name in args_names])
-			output_map[param] = function(**args_to_values)
-
-		return output_map
+		return compute_from_top_order(top_order, name_to_func, input_map)
 
 	return compute_all
 
 
+def lazy_compile(functions):
+	"""
+	Returns a function compute_given.
+	The compute_given accepts a map of input parameters, iterable of parameters that have to be computed
+	and returns a map of computed parameters.
+	Each function parameter name must match the name of the function that computes this parameter.
+	Can raise CyclicDependency and MissingParameter.
+	"""
+
+	name_to_func = map_names_to_functions(functions)
+	graph = make_graph(functions)
+	# we will compute top_order only when we know what params have to be computed
+
+	def compute_given(input_map, params_to_compute):
+
+		top_order = reversed_top_order(graph, params_to_compute)
+		return compute_from_top_order(top_order, name_to_func, input_map)
+
+	return compute_given
+
+
+def map_names_to_functions(functions):
+	"""
+	Service function that returns dict{function_name -> function}
+	"""
+
+	return dict([(func.__name__, func) for func in functions])
+
+
 def make_graph(functions):
 	"""
-	Makes dictionary of function name -> function arguments
+	Service function that return dict {function_name -> function_arguments}
 	"""
 
-	graph = {}
-	for function in functions:
-		graph[function.__name__] = function.func_code.co_varnames
-
-	return graph
+	return dict([(func.__name__, func.func_code.co_varnames) for func in functions])
 
 
-class CyclicDependancy(Exception):
-	pass
-
-
-def reversed_top_sort(graph):
+def reversed_top_order(graph, nodes_to_explore):
 	"""
 	Returns a reversed list of topologically sorted nodes of the graph.
 	Raises CyclicDependancy if graph has cycles.
 	"""
 
-	top_sorted_list = list()
-	top_sorted_set = set() # this is just for performance. To quickly find topologically sorted nodes
-	pending = list(graph) # stack that holds nodes to perform dfs
+	top_order = list()
+	completed = set() # this is just for performance. To quickly find topologically sorted nodes.
+	pending = list(nodes_to_explore) # stack that holds nodes to perform dfs
 	visited = set()
 	while pending:
 
 		node = pending[-1]
 
 		# if node is already topologically sorted - just remove it and continue with next node
-		if node in top_sorted_set:
+		if node in completed:
 			del pending[-1]
 			continue
 			
@@ -83,20 +100,47 @@ def reversed_top_sort(graph):
 			next_nodes = graph[node]
 			for next_node in next_nodes:
 				if next_node in visited:
-					raise CyclicDependancy()
-				if next_node not in top_sorted_set:
+					raise CyclicDependency()
+				if next_node not in completed:
 					pending.append(next_node)
 					break
 
-		# if no new node was added to pending list - this node is sorted
+		# if no new node was added - add this node to top_order
 		if pending[-1] == node:
-			top_sorted_list.append(node)
-			top_sorted_set.add(node)
+			top_order.append(node)
+			completed.add(node)
 			visited.remove(node)
 			del pending[-1]
 
 
-	return top_sorted_list
+	return top_order
+
+
+def compute_from_top_order(top_order, name_to_func, input_map):
+	"""
+	Service function that computes all parameters in top_order.
+	name_to_func - is a dict {function_name: function}
+	input_map - is a dict {parameter_name: parameter_value}
+	"""
+
+	output_map = input_map.copy()
+
+	for param in top_order:
+
+		# if value of the parameter was already given - continue
+		if param in output_map:
+			continue
+
+		# if there is no function to compute parameter - raise MissingParameter
+		if param not in name_to_func:
+			raise MissingParameter(param)
+
+		function = name_to_func[param]
+		args_names = function.func_code.co_varnames
+		args_to_values = dict([(arg_name, output_map[arg_name]) for arg_name in args_names])
+		output_map[param] = function(**args_to_values)
+
+	return output_map
 
 
 if __name__ == '__main__':
@@ -109,21 +153,37 @@ if __name__ == '__main__':
 		return float(sum(xs)) / n
 
 	def m2(xs, n):
+		global m2_was_called
+		m2_was_called = True
 		return float(sum(map(lambda x: x*x, xs))) / n
 
 	def v(m, m2):
+		global v_was_called
+		v_was_called = True
 		return m2 - m*m
 
 	functions = (n, m, m2, v)
+
+
+	# Simple compile
 	compute_all = simple_compile(functions)
 	output_map = compute_all({'xs': [1, 2, 3, 6]})
-
 	assert output_map['n'] == 4
 	assert output_map['m'] == 3
 	assert output_map['m2'] == 12.5
 	assert output_map['v'] == 3.5
 
-	# Cyclic dependancy
+
+	# Missing parameter
+	try:
+		compute_all({'ys': [1, 2, 3, 6]})
+		# must raise MissingParameter, i.e. must not reach this row
+		assert False
+	except MissingParameter:
+		pass
+
+
+	# Cyclic dependency
 	def x(y):
 		pass
 
@@ -137,5 +197,16 @@ if __name__ == '__main__':
 		simple_compile((x, y, z))
 		# must raise CyclicDependancy, i.e. must not reach this row
 		assert False
-	except CyclicDependancy:
+	except CyclicDependency:
 		pass
+
+
+	# Lazy compile
+	m2_was_called = False
+	v_was_called = False
+	compute_given = lazy_compile(functions)
+	output_map = compute_given({'xs': [1, 2, 3, 6]}, 'm')
+	assert output_map['n'] == 4
+	assert output_map['m'] == 3
+	assert not m2_was_called
+	assert not v_was_called
